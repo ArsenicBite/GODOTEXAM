df4f73e46a7f9b4bd56df56a7004831281554cad
Arsenic <81708177+ArsenicBite@users.noreply.github.com>
2025-05-15 12:51:41 +0100

    Score will display based on score variable

---
+++ godotexam/collision_shape_2d.gd
@@ -0,0 +1,9 @@
+extends CollisionShape2D
+@export var speed = 100
+var fish = true
+
+
+
+
+func _process(delta):	
+	position +1
--- godotexam/player.gd
+++ godotexam/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var score = 0
+
+func _process(delta: float) -> void:
+	$"../Label".text = "SCORE: " + str(score)
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
